26a6c62678d9cd53528168f31fadc7af012e1026
Chaz Wilson <charlessumnerwilson@gmail.com>
2019-06-25 14:58:21 +0900

    Failing tests for two digit numerals (10, 11, 19).

--- test/arabic2english-test.js
+++ test/arabic2english-test.js
@@ -1,11 +1,23 @@
 const assert = require("chai").assert;
 const app = require("../arabic2english");
 
-describe("Arabic numerals to english conversion", function() {
-  it("givin the input 1 function should return one", function() {
+describe("Convert single digit Arabic numerals to English", function() {
+  it("Given the input 1, function should return one", function() {
     assert.equal(app(1), "one");
   });
-  it("givin the input 7 function should return seven", function() {
-    assert.equal(app(7), "seven");
+  it("Given the input 9, function should return nine", function() {
+    assert.equal(app(9), "nine");
+  });
+});
+
+describe("Convert two digit Arabic numerals to English", function() {
+  it("Given the input 10, function should return ten", function() {
+    assert.equal(app(10), "ten");
+  });
+  it("Given the input 11, function should return eleven", function() {
+    assert.equal(app(11), "eleven");
+  });
+  it("Given the input 19, function should return nineteen", function() {
+    assert.equal(app(19), "nineteen");
   });
 });

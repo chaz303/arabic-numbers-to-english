df1dd444a5c7c21c292d2aff12e5f23fcb29fc41
Chaz Wilson <charlessumnerwilson@gmail.com>
2019-06-25 14:56:04 +0900

    Passing tests for single digit numerals.

--- test/arabic2english-test.js
+++ test/arabic2english-test.js
@@ -5,4 +5,7 @@ describe("Arabic numerals to english conversion", function() {
   it("givin the input 1 function should return one", function() {
     assert.equal(app(1), "one");
   });
+  it("givin the input 7 function should return seven", function() {
+    assert.equal(app(7), "seven");
+  });
 });

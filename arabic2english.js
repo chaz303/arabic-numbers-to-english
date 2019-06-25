ff3773dc6e03f146745de6f33330b6677b5fce51
Chaz Wilson <charlessumnerwilson@gmail.com>
2019-06-25 15:11:01 +0900

    Passing tests for two digit numbers 10-19

--- arabic2english.js
+++ arabic2english.js
@@ -1,5 +1,7 @@
 function singleDigit(digit) {
   switch (digit) {
+    case 0:
+      return "zero";
     case 1:
       return "one";
     case 2:
@@ -21,8 +23,37 @@ function singleDigit(digit) {
   }
 }
 
+function doubleDigit(digit) {
+  switch (digit) {
+    case 10:
+      return "ten";
+    case 11:
+      return "eleven";
+    case 12:
+      return "twelve";
+    case 13:
+      return "thirteen";
+    case 14:
+      return "fourteen";
+    case 15:
+      return "fifteen";
+    case 16:
+      return "sixteen";
+    case 17:
+      return "seventeen";
+    case 18:
+      return "eighteen";
+    case 19:
+      return "nineteen";
+  }
+}
+
 function conversion(digit) {
-  return singleDigit(digit);
+  if (digit <= 9) {
+    return singleDigit(digit);
+  } else if (digit <= 19) {
+    return doubleDigit(digit);
+  }
 }
 
 module.exports = conversion;
